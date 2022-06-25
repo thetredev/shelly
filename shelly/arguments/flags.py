ca5fed178e0b64a6a35c96868710dde28218ef71
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 17:25:35 +0200

    arguments/flags/ShellArgumentFlag._parse: More verbose variable names
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/flags.py
+++ shelly/arguments/flags.py
@@ -16,11 +16,11 @@ class ShellArgumentFlag(ShellArgumentBase):
         #  Command line: -v -v
         #  Result: 2
 
-        args = [arg for arg in command_line if self._is_flag(arg)]
+        arguments = [argument for argument in command_line if self._is_flag(argument)]
 
         self._value.data = sum([
-            arg.count(self.__identifier) for arg in args
-            if all(identifier == self.__identifier for identifier in arg[1:])
+            argument.count(self.__identifier) for argument in arguments
+            if all(identifier == self.__identifier for identifier in argument[1:])
         ])
 
     def _is_flag(self, arg: str):
