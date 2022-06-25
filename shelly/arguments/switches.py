ec34f89e2bca8dac68629912edbdbffc59adf5b0
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 17:31:37 +0200

    arguments/switches/ShellArgumentSwitch: Add documentation
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/switches.py
+++ shelly/arguments/switches.py
@@ -5,9 +5,18 @@ from shelly.arguments.base import ShellArgumentBase
 
 
 class ShellArgumentSwitch(ShellArgumentBase):
+    """Subclass used to implement parsing argument switches."""
+
+    # The default argument value delimiter
     delimiter: str = "="
 
     def _parse(self) -> None:
+        """Parsing implementation."""
+        # Example:
+        #  Key: --verbosity
+        #  Command line: --verbosity=4
+        #  Result: 4
+
         with contextlib.suppress(StopIteration):
             arg_value = command_line[self.key_index]
             self._value.data = self.value_type(arg_value.split(self.delimiter)[1])
