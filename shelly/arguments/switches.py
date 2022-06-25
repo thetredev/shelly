5e3652263f5fef6726351f29f703d1e211b09ad9
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 15:07:38 +0200

    arguments/switches/ShellArgumentSwitch: Add remaining type hints
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/switches.py
+++ shelly/arguments/switches.py
@@ -3,8 +3,8 @@ from shelly.arguments.cli import command_line
 
 
 class ShellArgumentSwitch(ShellArgumentBase):
-    delimiter = "="
+    delimiter: str = "="
 
-    def _parse(self):
+    def _parse(self) -> None:
         arg_value = command_line[self.key_index]
         self._value.data = self.value_type(arg_value.split(self.delimiter)[1])
