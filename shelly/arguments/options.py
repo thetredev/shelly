828be9aa9a043accc506a9a9894e909bec98c607
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 17:26:46 +0200

    arguments/options/ShellArgumentOption: Add documentation
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/options.py
+++ shelly/arguments/options.py
@@ -5,7 +5,15 @@ from shelly.arguments.base import ShellArgumentBase
 
 
 class ShellArgumentOption(ShellArgumentBase):
+    """Subclass used to implement parsing argument options."""
+
     def _parse(self):
+        """Parsing implementation."""
+        # Example:
+        #  Key: -x
+        #  Command line: -x 70
+        #  Result: 70
+
         with contextlib.suppress(StopIteration):
             arg_index = self.key_index + 1
             self._value.data = self.value_type(command_line[arg_index])
