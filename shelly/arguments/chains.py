139cefbcc70a9d71bd69a973249da12f6b98d9a9
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 17:14:26 +0200

    arguments/chains/ShellArgumentChain: Add documentation
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/chains.py
+++ shelly/arguments/chains.py
@@ -5,7 +5,15 @@ from shelly.arguments.cli import command_line
 
 
 class ShellArgumentChain(ShellArgumentBase):
+    """Subclass used to implement parsing argument chains."""
+
     def _parse(self) -> None:
+        """Parsing implementation."""
+        # Example:
+        #  Key: -z
+        #  Command line: -z 1 -z 2 -z 3
+        #  Result: [1, 2, 3]
+
         with contextlib.suppress(StopIteration):
             self._value.data = [
                 self.value_type(command_line[key_index + 1]) for key_index in self.key_indices
