d8dc1393b19e29740a849ebce4e9ff30bfdaf7f0
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 20:19:52 +0200

    arguments/chains/ShellArgumentChain: Convert class to dataclass
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/chains.py
+++ shelly/arguments/chains.py
@@ -1,7 +1,10 @@
+from dataclasses import dataclass
+
 from shelly.cli import command_line
 from shelly.arguments.base import ShellArgumentBase
 
 
+@dataclass(frozen=True, eq=True, slots=True)
 class ShellArgumentChain(ShellArgumentBase):
     """Subclass used to implement parsing argument chains."""
 
