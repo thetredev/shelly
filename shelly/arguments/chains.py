cc7360ab09f1b7c64791e44ea2ac97d2ee5f36a0
Timo Reichl <timo.reichl@mailbox.org>
2022-06-25 14:38:48 +0200

    arguments/chains/ShellArgumentChain: Add remaining type hints
    Signed-off-by: Timo Reichl <timo.reichl@mailbox.org>

--- shelly/arguments/chains.py
+++ shelly/arguments/chains.py
@@ -3,7 +3,7 @@ from shelly.arguments.cli import command_line
 
 
 class ShellArgumentChain(ShellArgumentBase):
-    def _parse(self):
+    def _parse(self) -> None:
         self._value.data = [
             self.value_type(command_line[key_index + 1]) for key_index in self.key_indices
         ]
